cef9d6b9af6f9fd8c42d2822d198c1535b5fa49a
CuriouslyCory <cory@hau.me>
2023-02-28 16:27:33 -0700

    Socials are readonly

--- packages/constants/src/socials.ts
+++ packages/constants/src/socials.ts
@@ -5,10 +5,10 @@ export const socialLinks = [
     title: "Twitter",
     href: "https://twitter.com/",
     Icon: FaTwitter,
-  },
+  } as const,
   {
     title: "Discord",
     href: "https://discord.gg/",
     Icon: FaDiscord,
-  },
-];
+  } as const,
+] as const;
